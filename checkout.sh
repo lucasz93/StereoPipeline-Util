575fc28d24f4e03748330e3e6230de3972b71f4e
Lucas Zadrozny <lucas.zadrozny@hotmail.com>
2021-07-07 21:55:01 +1000

    Made scripts Windows compatible

--- checkout.sh
+++ checkout.sh
@@ -3,27 +3,24 @@ buildDir=`pwd`/..
 installDir=$buildDir/install
 cd $buildDir
 
-$CONDA_PREFIX/envs/asp_deps/bin/git clone                  \
-    https://github.com/lucasz93/visionworkbench.git
+git clone https://github.com/lucasz93/visionworkbench.git
 pushd visionworkbench
 git checkout 2.7.0_omp
 git submodule update --init --recursive
 popd
 
 
-$CONDA_PREFIX/envs/asp_deps/bin/git clone                  \
-https://github.com/lucasz93/BinaryBuilder.git
+git clone https://github.com/lucasz93/BinaryBuilder.git
 
 
-$CONDA_PREFIX/envs/asp_deps/bin/git clone                  \
-https://github.com/lucasz93/ISIS3.git
+git clone https://github.com/lucasz93/ISIS3.git
 pushd ISIS3
 git checkout 4.4_omp
 git submodule update --init --recursive
 popd
 
-$CONDA_PREFIX/envs/asp_deps/bin/git clone                  \
-https://github.com/lucasz93/StereoPipeline.git
+
+git clone https://github.com/lucasz93/StereoPipeline.git
 pushd StereoPipeline
 git checkout 2.7.0_omp
 git submodule update --init --recursive
