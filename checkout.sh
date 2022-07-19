692c424530cd72372e8db4961be4f6716aac6470
Lucas Zadrozny <lucas.zadrozny@hotmail.com>
2022-07-20 01:53:51 +1000

    Modified checkout.sh to also fetch the remote master

--- checkout.sh
+++ checkout.sh
@@ -7,14 +7,35 @@ cd $buildDir
 checkout_branch()
 {
 	url=$1
-	out_dir=$2
-	branch=$3
+	upstream=$2
+	out_dir=$3
+	branch=$4
 
-	if [ ! -d $2 ]; then
+	if [ ! -d $out_dir ]; then
 		git clone $url
 		pushd $out_dir
-			git checkout $branch
-			git submodule update --init --recursive
+		
+		if [ ! -z "$upstream" ]; then
+			git remote add upstream $upstream
+		fi
+		
+		git checkout $branch
+		git submodule update --init --recursive
+		
+		popd
+	fi
+	
+	if [ ! -z "$upstream" ]; then
+		pushd $out_dir
+		
+		# Some SJW chucklefucks decided the term 'master' was offensive, so now we need to manually detect the master branch name.
+		upstream_master_branch_name=`git remote show upstream | sed -n '/HEAD branch/s/.*: //p'`
+		origin_branch_name=`git remote show origin | sed -n '/HEAD branch/s/.*: //p'`
+		
+		git fetch upstream $upstream_master_branch_name:$origin_branch_name --update-head-ok
+		git push origin $origin_branch_name
+		git push --tags
+		
 		popd
 	fi
 }
@@ -22,27 +43,27 @@ checkout_branch()
 #-------------------------------------------------------------------------------
 # NAIF TOOLKIT
 #-------------------------------------------------------------------------------
-checkout_branch "https://github.com/lucasz93/f2c.git" "f2c" "global-state-object"
-checkout_branch "https://github.com/lucasz93/naif-toolkit.git" "naif-toolkit" "master"
-checkout_branch "https://github.com/lucasz93/cspice.git" "cspice" "libf2c-multithreading"
-checkout_branch "https://github.com/lucasz93/cspice-feedstock.git" "cspice-feedstock" "dev"
+checkout_branch "https://github.com/lucasz93/f2c.git" "https://github.com/barak/f2c" "f2c" "global-state-object"
+checkout_branch "https://github.com/lucasz93/naif-toolkit.git" "" "naif-toolkit" "master"
+checkout_branch "https://github.com/lucasz93/cspice.git" "" "cspice" "libf2c-multithreading"
+checkout_branch "https://github.com/lucasz93/cspice-feedstock.git" "https://github.com/conda-forge/cspice-feedstock" "cspice-feedstock" "dev"
 
 #-------------------------------------------------------------------------------
 # CUSTOM COMPILED DEPENDENCIES
 #-------------------------------------------------------------------------------
-checkout_branch "https://github.com/lucasz93/SpiceyPy.git" "SpiceyPy" "2.3_naif_context"
-checkout_branch "https://github.com/lucasz93/spiceypy-feedstock.git" "spiceypy-feedstock" "2.3.2_local"
+checkout_branch "https://github.com/lucasz93/SpiceyPy.git" "https://github.com/AndrewAnnex/SpiceyPy" "SpiceyPy" "2.3_naif_context"
+checkout_branch "https://github.com/lucasz93/spiceypy-feedstock.git" "https://github.com/conda-forge/spiceypy-feedstock" "spiceypy-feedstock" "2.3.2_local"
 
-checkout_branch "https://github.com/lucasz93/ale.git" "ale" "0.8.5-naif_context"
-checkout_branch "https://github.com/lucasz93/ale-feedstock.git" "ale-feedstock" "0.8.5-naif_context"
+checkout_branch "https://github.com/lucasz93/ale.git" "https://github.com/USGS-Astrogeology/ale" "ale" "0.8.5-naif_context"
+checkout_branch "https://github.com/lucasz93/ale-feedstock.git" "https://github.com/conda-forge/ale-feedstock" "ale-feedstock" "0.8.5-naif_context"
 
-checkout_branch "https://github.com/lucasz93/isis3_dependencies.git" "isis3_dependencies" "dev"
+#checkout_branch "https://github.com/lucasz93/isis3_dependencies.git" "https://github.com/chrisryancombs/isis3_dependencies" "isis3_dependencies" "dev"
 
 #-------------------------------------------------------------------------------
 # ACTUAL PROGRAMS
 #-------------------------------------------------------------------------------
-checkout_branch "https://github.com/lucasz93/BinaryBuilder.git" "BinaryBuilder" "master"
-checkout_branch "https://github.com/lucasz93/visionworkbench.git" "visionworkbench" "2.7.0_turbo-camera-forking"
-checkout_branch "https://github.com/lucasz93/ISIS3.git" "ISIS3" "4.1_turbo-multithreaded-naif"
-checkout_branch "https://github.com/lucasz93/StereoPipeline.git" "StereoPipeline" "2.7.0_turbo-camera-forking"
+checkout_branch "https://github.com/lucasz93/BinaryBuilder.git" "https://github.com/NeoGeographyToolkit/BinaryBuilder" "BinaryBuilder" "master"
+checkout_branch "https://github.com/lucasz93/visionworkbench.git" "https://github.com/visionworkbench/visionworkbench" "visionworkbench" "2.7.0_turbo-camera-forking"
+checkout_branch "https://github.com/lucasz93/ISIS3.git" "https://github.com/USGS-Astrogeology/ISIS3" "ISIS3" "4.1_turbo-multithreaded-naif"
+checkout_branch "https://github.com/lucasz93/StereoPipeline.git" "https://github.com/NeoGeographyToolkit/StereoPipeline" "StereoPipeline" "2.7.0_turbo-camera-forking"
 
